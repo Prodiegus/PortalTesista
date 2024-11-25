ea572689cc9d4758e5e78b2fc56727f5419c47b7
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-11-25 20:25:06 -0300

    setting up somethings

--- API-Portal-Tesista/server.js
+++ API-Portal-Tesista/server.js
@@ -1,7 +1,19 @@
 // server.js
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+// Configurar CORS
+app.use(cors({
+  //origin: ['https://34.176.220.92', 'https://localhost:4200'],
+  origin: ['*'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization'],
+  credentials: true
+}));
 
 // Middleware para parsear JSON
 app.use(express.json());
@@ -10,7 +22,13 @@ app.use(express.json());
 const routes = require('./routes');
 app.use('/api', routes);
 
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+// Leer los certificados SSL
+const options = {
+  key: fs.readFileSync('/etc/ssl/certs/selfsigned.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/selfsigned.crt')
+};
+
+// Iniciar el servidor HTTPS
+https.createServer(options, app).listen(port, () => {
+  console.log(`Servidor escuchando en https://localhost:${port}`);
 });
