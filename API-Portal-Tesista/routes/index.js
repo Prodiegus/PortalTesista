9b738c128646fb24b05977a23ed878cb71769354
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-11-25 20:41:54 -0300

    Problemas en el backend

--- API-Portal-Tesista/routes/index.js
+++ API-Portal-Tesista/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+  res.send('API de Portal Tesista');
 });
 
 module.exports = router;
