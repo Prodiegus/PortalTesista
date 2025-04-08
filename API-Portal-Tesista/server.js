793bbbea9c7cff4c91e96d19af8417807d2ee266
Prodiegus <81390806+Prodiegus@users.noreply.github.com>
2025-04-08 17:24:06 -0400

    Enhance file upload handling by increasing JSON and URL-encoded body size limits; refactor PDF file processing to use FileReader for base64 encoding

--- API-Portal-Tesista/server.js
+++ API-Portal-Tesista/server.js
@@ -3,6 +3,7 @@ const https = require('https');
 const fs = require('fs');
 const cors = require('cors');
 const config = require('./config');
+const bodyParser = require('body-parser');
 const app = express();
 const winston = require('winston');
 const DailyRotateFile = require('winston-daily-rotate-file');
@@ -72,7 +73,8 @@ app.use(cors({
 }));
 
 // Middleware para parsear JSON
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 // Importar rutas
 const routes = require('./routes');
