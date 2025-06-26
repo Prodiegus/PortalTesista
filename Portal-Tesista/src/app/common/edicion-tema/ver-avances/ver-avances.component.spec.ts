f0e3c29a2e9384c16d587e59acb1c21012b0a07e
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2025-06-26 13:46:10 -0400

    commit antes del merge

--- Portal-Tesista/src/app/common/edicion-tema/ver-avances/ver-avances.component.spec.ts
+++ Portal-Tesista/src/app/common/edicion-tema/ver-avances/ver-avances.component.spec.ts
@@ -1,64 +1,57 @@
 import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
-import { VerAvancesComponent } from './ver-avances.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { VerAvancesComponent, Profesor } from './ver-avances.component';
 import { HttpRequestService } from '../../Http-request.service';
 import { of, throwError } from 'rxjs';
 
-describe('VerAvancesComponent', () => { afterEach(() => { TestBed.resetTestingModule(); });
+describe('VerAvancesComponent', () => {
   let component: VerAvancesComponent;
   let fixture: ComponentFixture<VerAvancesComponent>;
-  let httpRequestServiceSpy: jasmine.SpyObj<HttpRequestService>;
+  let httpSpy: jasmine.SpyObj<HttpRequestService>;
 
-  const mockTema = { id: 1 };
-  const mockUser = { tipo: 'cargo', escuela: 'TI', rut: '12345678-9' };
+  const temaMock = { id: 1 };
+  const userMock = { tipo: 'cargo', escuela: 'Esc1', rut: 'R1' };
+  const avanceMock = { archivo: 'data:application/pdf;base64,QUJD', tipoMime: 'application/pdf', nombre_archivo: 'doc.pdf' };
+  // Use Profesor interface shape
+  const profesorItems: Profesor[] = [{ nombre: 'P A', rut: 'R2', email: 'p@e.com' }];
+  const revisoresData = [...profesorItems];
 
   beforeEach(async () => {
-    const spy = jasmine.createSpyObj('HttpRequestService', [
-      'getAvancesTema',
-      'getProfesores',
-      'getRevisoresTema',
-      'addRevisor',
-      'borrarRevisor'
+    httpSpy = jasmine.createSpyObj('HttpRequestService', [
+      'getAvancesTema', 'getProfesores', 'getRevisoresTema', 'addRevisor', 'borrarRevisor'
     ]);
 
     await TestBed.configureTestingModule({
       declarations: [VerAvancesComponent],
-      imports: [HttpClientTestingModule],
-      providers: [
-        { provide: HttpRequestService, useValue: spy }
-      ]
+      providers: [{ provide: HttpRequestService, useValue: httpSpy }]
     }).compileComponents();
 
-    httpRequestServiceSpy = TestBed.inject(HttpRequestService) as jasmine.SpyObj<HttpRequestService>;
     fixture = TestBed.createComponent(VerAvancesComponent);
     component = fixture.componentInstance;
-    component.tema = mockTema;
-    component.userRepresentation = mockUser;
+    component.tema = temaMock;
+    component.userRepresentation = userMock;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should fetch avances, profesores, and revisores on init', fakeAsync(() => {
-    httpRequestServiceSpy.getAvancesTema.and.returnValue(Promise.resolve(of([])));
-    httpRequestServiceSpy.getProfesores.and.returnValue(Promise.resolve(of([])));
-    httpRequestServiceSpy.getRevisoresTema.and.returnValue(Promise.resolve(of([])));
+  it('should create and init', fakeAsync(() => {
+    httpSpy.getAvancesTema.and.returnValue(Promise.resolve(of([avanceMock])));
+    httpSpy.getProfesores.and.returnValue(Promise.resolve(of(profesorItems)));
+    httpSpy.getRevisoresTema.and.returnValue(Promise.resolve(of(revisoresData)));
 
     component.ngOnInit();
     tick();
 
     expect(component.loading).toBeFalse();
-    expect(httpRequestServiceSpy.getAvancesTema).toHaveBeenCalledWith(1);
-    expect(httpRequestServiceSpy.getProfesores).toHaveBeenCalledWith('TI');
-    expect(httpRequestServiceSpy.getRevisoresTema).toHaveBeenCalledWith(1);
+    expect((component as any).avances).toEqual([avanceMock]);
+    expect((component as any).profesores).toEqual(profesorItems);
+    expect((component as any).revisores).toEqual(revisoresData);
+    expect((component as any).dataSource.length).toBe(1);
     expect(component.esCargo).toBeTrue();
   }));
 
-  xit('should handle error in fetchAvances', fakeAsync(() => {
-    httpRequestServiceSpy.getAvancesTema.and.returnValue(Promise.resolve(of(throwError(() => new Error('Error')))));
-    httpRequestServiceSpy.getProfesores.and.returnValue(Promise.resolve(of([])));
-    httpRequestServiceSpy.getRevisoresTema.and.returnValue(Promise.resolve(of([])));
+  it('should handle error in fetchAvances', fakeAsync(() => {
+    httpSpy.getAvancesTema.and.returnValue(Promise.resolve(throwError(() => new Error('e'))));
+    httpSpy.getProfesores.and.returnValue(Promise.resolve(of([])));
+    httpSpy.getRevisoresTema.and.returnValue(Promise.resolve(of([])));
+    spyOn(console, 'error');
 
     component.ngOnInit();
     tick();
@@ -67,44 +60,72 @@ describe('VerAvancesComponent', () => { afterEach(() => { TestBed.resetTestingMo
     expect(component.loading).toBeFalse();
   }));
 
-  it('should not add revisor if profesorSeleccionado is undefined', fakeAsync(() => {
+  it('should not sacarRevisor if no element', fakeAsync(() => {
+    spyOn(console, 'error');
+    component.sacarRevisor(null as any);
+    tick();
+    expect(console.error).toHaveBeenCalledWith('Elemento no seleccionado');
+  }));
+
+  it('should sacarRevisor success and error', fakeAsync(() => {
+    httpSpy.borrarRevisor.and.returnValue(Promise.resolve(of(revisoresData)));
+    httpSpy.getRevisoresTema.and.returnValue(Promise.resolve(of(revisoresData)));
+    component.sacarRevisor(profesorItems[0]);
+    tick();
+    expect(httpSpy.borrarRevisor).toHaveBeenCalled();
+    expect(httpSpy.getRevisoresTema).toHaveBeenCalledWith(1);
+
+    httpSpy.borrarRevisor.and.returnValue(Promise.resolve(throwError(() => new Error('e2'))));
+    spyOn(console, 'error');
+    component.sacarRevisor(profesorItems[0]);
+    tick();
+    expect(console.error).toHaveBeenCalledWith('Error al eliminar revisor');
+  }));
+
+  it('should not agregarRevisor if none selected', fakeAsync(() => {
+    spyOn(console, 'error');
     component.profesorSeleccionado = undefined;
-    const consoleSpy = spyOn(console, 'error');
-
     component.agregarRevisor();
     tick();
-
-    expect(consoleSpy).toHaveBeenCalledWith('Ningún profesor seleccionado');
+    expect(console.error).toHaveBeenCalledWith('Ningún profesor seleccionado');
   }));
 
-  it('should call addRevisor and fetchRevisores when agregarRevisor is valid', fakeAsync(() => {
-    component.profesorSeleccionado = { rut: '1111' };
-    httpRequestServiceSpy.addRevisor.and.returnValue(Promise.resolve(of([])));
-    httpRequestServiceSpy.getRevisoresTema.and.returnValue(Promise.resolve(of([])));
-
+  it('should agregarRevisor success and error', fakeAsync(() => {
+    component.profesorSeleccionado = profesorItems[0];
+    httpSpy.addRevisor.and.returnValue(Promise.resolve(of(revisoresData)));
+    httpSpy.getRevisoresTema.and.returnValue(Promise.resolve(of(revisoresData)));
     component.agregarRevisor();
     tick();
+    expect(httpSpy.addRevisor).toHaveBeenCalled();
+    expect(httpSpy.getRevisoresTema).toHaveBeenCalledWith(1);
 
-    expect(httpRequestServiceSpy.addRevisor).toHaveBeenCalled();
-    expect(httpRequestServiceSpy.getRevisoresTema).toHaveBeenCalledWith(1);
-  }));
-
-  it('should handle sacarRevisor properly', fakeAsync(() => {
-    const mockRevisor = { rut: '12345678-9', nombre: 'Juan Perez', email: 'jp@utalca.cl' };
-    httpRequestServiceSpy.borrarRevisor.and.returnValue(Promise.resolve(of([])));
-    httpRequestServiceSpy.getRevisoresTema.and.returnValue(Promise.resolve(of([])));
-
-    component.sacarRevisor(mockRevisor);
+    httpSpy.addRevisor.and.returnValue(Promise.resolve(throwError(() => new Error('e3'))));
+    spyOn(console, 'error');
+    component.agregarRevisor();
     tick();
-
-    expect(httpRequestServiceSpy.borrarRevisor).toHaveBeenCalled();
-    expect(httpRequestServiceSpy.getRevisoresTema).toHaveBeenCalledWith(1);
+    expect(console.error).toHaveBeenCalledWith('Error al agregar revisor');
   }));
 
-  it('should not descargarAvance if archivo is missing', () => {
-    const consoleSpy = spyOn(console, 'error');
-    component.descargarAvance({});
-    expect(consoleSpy).toHaveBeenCalledWith('No file available to download');
+  it('should abrir and cerrar avance', () => {
+    component.abrir_avance(avanceMock);
+    expect(component.ver_avance).toBeTrue();
+    expect(component.avanceSeleccionado).toBe(avanceMock);
+    component.cerrar_avance();
+    expect(component.ver_avance).toBeFalse();
   });
-});
 
+  it('should not descargarAvance if missing', () => {
+    spyOn(console, 'error');
+    component.descargarAvance({});
+    expect(console.error).toHaveBeenCalledWith('No file available to download');
+  });
+
+  it('should descargarAvance success and error', () => {
+    spyOn(document, 'createElement').and.returnValue({ click: () => {}, href: '', download: '' } as any);
+    spyOn(URL, 'createObjectURL').and.returnValue('url');
+    spyOn(URL, 'revokeObjectURL');
+    component.descargarAvance(avanceMock);
+    expect(URL.createObjectURL).toHaveBeenCalled();
+  });
+
+});
